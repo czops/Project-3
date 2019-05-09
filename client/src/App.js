5bc6b2e0e58724db86826f08451adf1a3d50f587
czops <37029225+czops@users.noreply.github.com>
2019-05-08 21:41:29 -0500

    masterState
    created the basic callback for editing masterState and passing it along through the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import AppIntro from "./components/AppIntro";
+
+import AppIntro from "./components/AppIntro";
 import Login from "./pages/Login/Login";
 import Measurements from "./pages/Measurements/Measurements"
 import NavbarComponent from "./components/navbar/Navbar";
 import NotFound from "./pages/NotFound/NotFound";
 import Process1 from "./pages/Process1/Process1"
 import Process2 from "./pages/Process2/Process2"
-import React from "react";
+import React, { Component} from "react";
 import Success from "./pages/Success/Success"
 
-function App() {
+class App extends Component {
+  // return <AppIntro />;
+
+  state = {
+    loggedIn: false
+  }
+  
+  setMasterState = ( key, value) => {
+
+    const newState = {};
+    newState[key] = value;
+
+    this.setState(newState);
+  }
+
+render(){
   return (
+    
     <Router>
       <div>
         <NavbarComponent />
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route exact path="/Measurements" component={Measurements} />
+          {/* <Route exact path="/LandingPage" render={(state) => <LandingPage {...state}/>} /> */}
+          <Route exact path="/Measurements" render={(state) => <Measurements masterState={this.state} setMasterState={this.setMasterState} />} />
           <Route exact path="/Process1" component={Process1} />
           <Route exact path="/Process2" component={Process2} />
           <Route exact path="/Success" component={Success} />
@@ -26,6 +44,6 @@ function App() {
       </div>
     </Router>
   );
-}
+}}
 
 export default App;
--- client/src/pages/Measurements/Measurements.js
+++ client/src/pages/Measurements/Measurements.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "react-bootstrap";
 
+
 class Measurements extends Component {
 
-
+    // state = {
+    //     // loggedIn: false
+    //     process1M1: "",
+    //     process2M2: ""
+    // }
 
     render() {
         return (
             <Container>
+
                 <Row>
                     <Col>
                         <h1>Measurements</h1>
+                        { JSON.stringify( this.props.masterState )}
+                        <button onClick={() => {
+                            this.props.setMasterState( 'measurement', true );
+                        }}>test</button>
                     </Col>
+
                 </Row>
             </Container>
         )
