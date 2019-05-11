7858c68c2642989b1bfa38923f592d29a1d7b050
czops <37029225+czops@users.noreply.github.com>
2019-05-11 14:06:21 -0500

    process1 is now a dumb component

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,23 @@ import Measurements from "./pages/Measurements/Measurements"
 import NotFound from "./pages/NotFound/NotFound";
 import Process1 from "./pages/Process1/Process1"
 import Process2 from "./pages/Process2/Process2"
-import React, { Component} from "react";
+import React, { Component } from "react";
 import Success from "./pages/Success/Success"
+
+import API from "./utils/API";
 
 class App extends Component {
   // return <AppIntro />;
 
   state = {
     loggedIn: false,
-    panel: 1
+    process1M1: 0,
+    process1M2: 0
   }
-  
-  setMasterState = ( key, value) => {
+
+  /*Optimized version of the props below*/
+
+  setMasterState = (key, value) => {
 
     const newState = {};
     newState[key] = value;
@@ -27,31 +32,92 @@ class App extends Component {
     this.setState(newState);
   }
 
+  /*Edit the master state using the following props*/
+
   setPanelNumber = (number) => {
-    console.log("got here!!");
+    console.log("Number of panels");
+    console.log(number);
+    this.setMasterState('panels', number);
+  }
+
+  setModelNumber = (number) => {
+    console.log("Model number");
     console.log(number);
   }
-    
-render(){
-  return (
-    
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          {/* <Route exact path="/LandingPage" render={(state) => <LandingPage {...state}/>} /> */}
-          <Route exact path="/Landing" render={(state) => <Landing masterState={this.state} setMasterState={this.setMasterState} setPanelNumber={this.setPanelNumber} />} />
-          <Route exact path="/Process" component={Process} />
-          <Route exact path="/Measurements" render={(state) => <Measurements masterState={this.state} setMasterState={this.setMasterState} />} />
-          <Route exact path="/Process1" component={Process1} />
-          <Route exact path="/Process2" component={Process2} />
-          <Route exact path="/Success" component={Success} />
-          {/* Create route from api to pull all panels, for T&E */}
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}}
+
+  setShopOrderNumber = (number) => {
+    console.log("Shop order number");
+    console.log(number);
+  }
+
+  successPageLoad = () => {
+    console.log(this.Process1.state);
+    console.log(this.state);
+  }
+
+  process1CheckState = () => {
+    console.log(this.Process1.state);
+
+  }
+
+  /*Function props to be passed into Process1*/
+
+  componentDidMount() {
+    API.getAllPanels() //get all panels on page load and log to client console
+      .then((res) => {
+        console.log(res.data)
+      });
+    let shopOrder = this.state.shopOrder;
+    API.getOnePanel(shopOrder)
+      .then((panel) => {
+        console.log("panel----------------------------")
+        console.log(panel)
+      })
+  };
+
+  handleInputChange = (event) => {
+    debugger;
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  PUTsomeStuff = () => {
+    let Process1 = {
+      process1M1: this.state.process1M1,
+      process1M2: this.state.process1M2,
+      shopOrder: this.state.shopOrder
+    }
+    console.log(Process1);
+
+    API.processTwo(Process1)
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  render() {
+    return (
+
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            {/* <Route exact path="/LandingPage" render={(state) => <LandingPage {...state}/>} /> */}
+            <Route exact path="/Landing" render={(state) => <Landing masterState={this.state} setMasterState={this.setMasterState} setPanelNumber={this.setPanelNumber} />} />
+            <Route exact path="/Process" component={Process} />
+            <Route exact path="/Measurements" render={(state) => <Measurements masterState={this.state} setMasterState={this.setMasterState} />} />
+            <Route exact path="/Process1" render={(state) => <Process1 masterState={this.state} setMasterState={this.setMasterState} process1CheckState={this.process1CheckState} handleInputChange={this.handleInputChange} PUTsomeStuff={this.PUTsomeStuff} />} />
+            <Route exact path="/Process2" render={(state) => <Process2 masterState={this.state} setMasterState={this.setMasterState} />} />
+            <Route exact path="/Success" component={Success} />
+            {/* Create route from api to pull all panels, for T&E */}
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
