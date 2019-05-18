da3fe3b2ddab767b1a0d80e50f7b0ba2e8ceebf9
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-05-18 14:07:20 -0500

    working

--- client/src/components/DataEntry2/DataEntry2.js
+++ client/src/components/DataEntry2/DataEntry2.js
@@ -4,36 +4,36 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import "../DataEntry2/DataEntry2.css";
 
-function DataEntryComponent2 (props) {
+function DataEntryComponent2(props) {
     return (
         <div className="formBox">
-            
+
             <Form>
-            <Form.Text className="stepText">
-                STEP 3.
+                <Form.Text className="stepText">
+                    STEP 3.
             </Form.Text>
-            <Form.Text className="sizeTitle">
-                Enter the Measurements
+                <Form.Text className="sizeTitle">
+                    Enter the Measurements
             </Form.Text>
-            {props.panels.map(panel => 
-                <Form.Row className="measureBox">
-                <Form.Group as={Col} controlId="formModelNumber">
-                    <Form.Text className="processTitle"> PANEL {panel.panelNumber} <span className="panelNumberEnter"></span></Form.Text>
-                    <Form.Row>
-                        <Form.Group>
-                            <Form.Label>Measurement 1</Form.Label>
-                            <Form.Control onChange={(event) => props.setMeasurement1(event, panel.panelNumber)} value={panel.m1} placeholder="Enter #" className="measurement1"/>
+                {props.panels.map(panel =>
+                    <Form.Row className="measureBox">
+                        <Form.Group as={Col} controlId="formModelNumber">
+                            <Form.Text className="processTitle"> PANEL {panel.panelNumber} <span className="panelNumberEnter"></span></Form.Text>
+                            <Form.Row>
+                                <Form.Group>
+                                    <Form.Label>Measurement 1</Form.Label>
+                                    <Form.Control onChange={(event) => props.setMeasurement1(event, panel.panelNumber)} value={panel.m1} placeholder="Enter #" className="measurement1" />
+                                </Form.Group>
+                                <Form.Group className="secondMeasure">
+                                    <Form.Label>Measurement 2</Form.Label>
+                                    <Form.Control onChange={(event) => props.setMeasurement2(event, panel.panelNumber)} value={panel.m2} placeholder="Enter #" className="measurement2" />
+                                </Form.Group>
+
+                            </Form.Row>
                         </Form.Group>
-                        <Form.Group className="secondMeasure">
-                            <Form.Label>Measurement 2</Form.Label>
-                            <Form.Control onChange={(event) => props.setMeasurement2(event, panel.panelNumber)} value={panel.m2} placeholder="Enter #" className="measurement2"/>
-                        </Form.Group>
-                    
-                    </Form.Row>
-                    </Form.Group>
                     </Form.Row>
                 )}
-         
+
                 <div className="buttonBox">
                     <Button variant="primary" type="submit" className="submitButton" block>
                         SUBMIT
@@ -43,6 +43,7 @@ function DataEntryComponent2 (props) {
             </Form>
         </div>
     )
+}
 
 
 export default DataEntryComponent2;
