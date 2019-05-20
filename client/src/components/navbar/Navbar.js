ee7f9f654b25650557aed703df87dc6b71453b7a
Sindy <sindy.gebhard@gmail.com>
2019-05-20 18:40:30 -0500

    making the navbar responsive
    making the navbar responsive

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "../navbar/navbar.css";
 //import ciruitLogo from "./navbar/logo.png";
 
 function NavbarComponent() {
     return (
-        <Navbar className="navbarBig">
-            <Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" className="navbarBig">
+        <Navbar.Brand>
                 <img
                     src="./images/logo.png"
                     width="230"
@@ -14,12 +15,14 @@ function NavbarComponent() {
                     className="d-inline-block align-top"
                     alt="Circuit Ninjas"
                 />
-            </Navbar.Brand>
-                <Navbar.Text className="logoutLink" pullRight>
-                    <a href="/"> | LOGOUT</a>
-                </Navbar.Text>
-        </Navbar>
-    )
-};
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+                <Nav.Link href="/" className="logoutLink">LOGOUT</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+)};
 
 export default NavbarComponent;
